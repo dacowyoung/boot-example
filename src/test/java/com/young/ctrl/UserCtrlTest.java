26d79a702b0ea47e1a757467906b63924f6af26d
dacowyoung <dailiuyang@yeah.net>
2020-06-18 09:37:59 +0800

    'clear_unuse_import'

--- src/test/java/com/young/ctrl/UserCtrlTest.java
+++ src/test/java/com/young/ctrl/UserCtrlTest.java
@@ -3,15 +3,12 @@ package com.young.ctrl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,6 +24,6 @@ public class UserCtrlTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("abc"));
     }
-    
-    
+
+
 }
